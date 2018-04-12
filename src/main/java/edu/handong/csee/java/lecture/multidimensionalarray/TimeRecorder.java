c2344ca676d767d24025cc4190a04c71e67ee96b
YuJin-Kim <21700140@handong.edu>
2018-04-12 22:19:53 +0900

    computeTatalPerWeekDay+computeTotalPerEmployee

--- src/main/java/edu/handong/csee/java/lecture/multidimensionalarray/TimeRecorder.java
+++ src/main/java/edu/handong/csee/java/lecture/multidimensionalarray/TimeRecorder.java
@@ -16,8 +16,7 @@ public class TimeRecorder {
 		
 		myTimeRecoder.getData();
 		
-		myTimeRecoder.computeTotalPerWeekDay();
-		myTimeRecoder.computeTotalPerEmployee();
+		myTimeRecoder.computeTotals();
 		
 		myTimeRecoder.printResults();
 
@@ -49,8 +48,10 @@ public class TimeRecorder {
 		myScanner.close();	
 	}
 	
-	public void computeTotalPerWeekDay() {
-			
+	public void computeTotals() {
+		
+		weekHours = new int[hours.length];
+		
 		for(WeekDays currentDay:WeekDays.values()) {
 			
 			dayHours[currentDay.ordinal()] = 0;
@@ -60,12 +61,7 @@ public class TimeRecorder {
 															+ hours[employeeCount][currentDay.ordinal()];
 			}
 		}
-	}
-	
-	public void computeTotalPerEmployee() {
-		
-		weekHours = new int[hours.length];
-		
+
 		for(int employeeCount=0; employeeCount < hours.length; employeeCount++) {
 			
 			weekHours[employeeCount] = 0;
@@ -75,7 +71,6 @@ public class TimeRecorder {
 															+ hours[employeeCount][currentDay.ordinal()];
 			}
 		}
-		
 	}
 	
 	public void printResults() {
